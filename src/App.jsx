fa3045566b877641b91978c151f90f52adfbd096
MahadSardar <mahadsardarm2@gmail.com>
2025-11-30 23:12:57 +0500

    files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Converterfrom from "./Components/Converterfrom.jsx";
+import Converterformprac from "./Components/Converterformprac.jsx";
+const App = () => {
+  return (
+    <div className="currency-converter">
+      <h2 className="converter-title">Currency Converter</h2>
+      <Converterformprac/>
+    </div>
+  );
+};
+
+export default App;
